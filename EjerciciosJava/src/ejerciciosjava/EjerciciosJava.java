e441327f30ec2ced80b9238195811905a0ebb76a
mykha <mykha@DESKTOP-U8AI2OO>
2018-03-03 16:29:01 +0100

    Prueba Git

---
+++ EjerciciosJava/src/ejerciciosjava/EjerciciosJava.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosjava;
+
+/**
+ *
+ * @author mykha
+ */
+public class EjerciciosJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre = "Mykhaylo Rishko";
+        int edad = 20;
+        double altura = 1.80;
+        System.out.println("Me llamo " + nombre+", tengo "+edad+" años, y mido "+altura);
+    }
+    
+}
